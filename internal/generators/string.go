989074c5f91c30eb36b141862c8d698e7fa0a700
Maks Kolman <maks@kolman.si>
2021-11-28 20:26:24 +0100

    Add option to define own charset for string generator

--- internal/generators/string.go
+++ internal/generators/string.go
@@ -6,26 +6,28 @@ import (
 	"github.com/bitstonks/syndi/internal/config"
 )
 
-// yaay, globals!
-var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
-
 type StringGenerator struct {
 	rng *rand.Rand
 	len int
+	all []rune
 }
 
 func NewStringGenerator(args config.Args) Generator {
-	// TODO: when defined use args.OneOf for character selection instead of global letters
+	all := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+	if len(args.OneOf) > 0 {
+		all = []rune(args.OneOf)
+	}
 	return &StringGenerator{
 		rng: NewRng(),
 		len: args.Length,
+		all: all,
 	}
 }
 
 func (g *StringGenerator) Next() string {
 	b := make([]rune, g.len)
 	for i := range b {
-		b[i] = letters[g.rng.Intn(len(letters))]
+		b[i] = g.all[g.rng.Intn(len(g.all))]
 	}
 	return string(b)
 }
