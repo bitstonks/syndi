d8d2694fa8cf01b20011c942eaa274e27829c4b0
Samo Ratnik <samo.ratnik@bitstamp.net>
2022-01-31 17:20:51 +0100

    Add a "container" for command-line args
    We want to split table definitions from run parameters, this is the
    first step in that.

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,11 +1,34 @@
 package config
 
 import (
+	"fmt"
 	"github.com/go-playground/validator/v10"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"log"
 )
+
+// RunArgs is a container for command-line flags passed in.
+type RunArgs struct {
+	Database string `validate:"required"`
+	Host     string `validate:"required"`
+	Password string `validate:"required"`
+	Port     string `validate:"required,number,gt=0"`
+	Safe     bool
+	Tables   []string `validate:"required,gt=0"`
+	User     string   `validate:"required"`
+}
+
+func (a RunArgs) GetDSN() string {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true&interpolateParams=true",
+		a.User,
+		a.Password,
+		a.Host,
+		a.Port,
+		a.Database,
+	)
+	return dsn
+}
 
 type Args struct {
 	Type     string  `yaml:"Type" validate:"required"`
--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -1,12 +1,65 @@
 package config
 
 import (
+	"github.com/go-playground/validator/v10"
 	"os"
 	"path"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func TestRunArgs(t *testing.T) {
+	t.Run("test initialization happy path", func(t *testing.T) {
+		args := RunArgs{
+			Database: "example",
+			Host:     "localhost",
+			Password: "root",
+			Port:     "3306",
+			Safe:     false,
+			Tables:   []string{"users.yaml", "accounts.yaml"},
+			User:     "root",
+		}
+		validate := validator.New()
+		err := validate.Struct(args)
+		assert.NoError(t, err)
+	})
+
+	t.Run("test initialization sad path", func(t *testing.T) {
+		args := RunArgs{
+			Database: "example",
+			Host:     "localhost",
+			Password: "root",
+			Port:     "invalid port number",
+			Safe:     false,
+			User:     "root",
+		}
+		validate := validator.New()
+		err := validate.Struct(args)
+		assert.EqualError(t, err, "Key: 'RunArgs.Port' Error:Field validation for 'Port' failed on the 'number' tag\nKey: 'RunArgs.Tables' Error:Field validation for 'Tables' failed on the 'required' tag")
+
+		args.Port = "3306"
+		err = validate.Struct(args)
+		assert.EqualError(t, err, "Key: 'RunArgs.Tables' Error:Field validation for 'Tables' failed on the 'required' tag")
+
+		args.Tables = []string{"users.yaml"}
+		err = validate.Struct(args)
+		assert.NoError(t, err)
+	})
+
+	t.Run("test GetDSN", func(t *testing.T) {
+		args := RunArgs{
+			Database: "example",
+			Host:     "localhost",
+			Password: "root",
+			Port:     "3306",
+			Safe:     false,
+			Tables:   []string{"users.yaml", "accounts.yaml"},
+			User:     "root",
+		}
+		assert.Equal(t, args.GetDSN(), "root:root@tcp(localhost:3306)/example?parseTime=true&interpolateParams=true")
+	})
+}
 
 func TestLoadConfig(t *testing.T) {
 	currWd, err := os.Getwd()
